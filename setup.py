df47b015ffa70b372315353576bab9eb71fec915
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-11-18 21:53:41 -0300

    Improve setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='meta_wrappers',
-    packages=find_packages(),
+    packages=find_packages(include=['meta_wrappers']),
     version='0.1.0',
     description='Easy-to-use wrappers for Meta chat platforms',
     author='TiagoCavalcante, sr-pato',
